d9c690ca269723328a256aff2b363f9dee00b86a
Kael.Cai <saint.archangel.satan@gmail.com>
2026-04-08 11:33:25 +0800

    feat: wrap Select component in AgentSelect to support dynamic option rendering

--- src/index.ts
+++ src/index.ts
@@ -67,8 +67,19 @@ export {
 
 // Unified UI Registry mapping tailored precisely for the Agent K Renderer
 // This maps the {"type": "Button"} JSON spec string to the actual React component.
+
+import React from 'react';
+// @ts-ignore
+export const AgentSelect = React.forwardRef(({ options, ...props }: any, ref) => {
+  
+  return React.createElement(Select, { ...props, ref }, 
+    options ? options.map((opt: any) => React.createElement(SelectItem, { key: opt.key }, opt.text)) : null
+  );
+
+});
+
 export const AgentFAtoms: Record<string, any> = {
-  Select,
+  Select: AgentSelect,
   SelectItem,
   NextUIProvider,
   Button,
